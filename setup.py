71d2de0585ab8cfada4f63704933c64b068d3848
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-04 18:56:52 +0100

    Add url tag

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     description="Python wrapper around mullvad api",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url='https://github.com/meichthys/mullvad-api',
     install_requires=["requests"],
     py_modules=["mullvad_api"],
     classifiers=[
